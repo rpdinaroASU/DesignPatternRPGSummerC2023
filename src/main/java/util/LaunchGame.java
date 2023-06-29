ba7143e98d48097d29e8e7c5c62afc8cc07e4b8b
rpdinaroASU <133998885+rpdinaroASU@users.noreply.github.com>
2023-06-29 14:42:58 -0600

    Initalized Checkstyle, Spotbug, jacoco in gradle

---
+++ src/main/java/util/LaunchGame.java
@@ -0,0 +1,32 @@
+package main.java.util;
+
+/**
+ * This class launches the game as it is called.
+ */
+public final class LaunchGame {
+    /**
+     * Establish a singleton.
+     */
+    private static LaunchGame game = null;
+
+    /**
+     * Calling this class will begin launching the RPG game,
+     * if an instance is already running, it will simply return
+     * the single instance.
+     * @return instance of LaunchGame
+     */
+    public static LaunchGame getInstance() {
+        if(game == null) {
+            game = new LaunchGame();
+        }
+        return game;
+    }
+
+    /**
+     * This constructor will initialize the game.
+     */
+    private LaunchGame() {
+
+    }
+
+}
---
+++ src/main/java/util/Main.java
@@ -0,0 +1,18 @@
+package main.java.util;
+
+public class Main {
+    /**
+     * Launcher of the game.
+     * @param args System args
+     */
+    public static void main(String[] args) {
+        System.out.println("Here");
+    }
+
+    /**
+     * resolves checksum error.
+     */
+    private Main() {
+
+    }
+}
