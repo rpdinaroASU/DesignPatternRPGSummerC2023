70b036cd35c26f2ec73ca925e2c0aadb82a824c1
rpdinaroASU <133998885+rpdinaroASU@users.noreply.github.com>
2023-06-29 15:49:46 -0600

    Add Item enum for character classes

--- src/main/java/characters/Player.java
+++ src/main/java/characters/Player.java
@@ -1,6 +1,10 @@
 package characters;
 
+import items.Item;
+
 public interface Player {
     public double addGold(int gain);
-    public double addGold(int gain);
+    public double removeGold(int gain);
+    public double addItem(Item foundItem, int slotNumber);
+    public double addItem(int removeItem);
 }
---
+++ src/main/java/items/Item.java
@@ -0,0 +1,37 @@
+package items;
+
+public enum Item {
+    PLACE_HOLDER(0,0,0,0,0);
+    private final int damage;
+    private final int intelligence;
+    private final int defence;
+    private final int resist;
+    private final int value;
+    Item(int damage, int intelligence, int defence, int resist, int value) {
+        this.damage = damage;
+        this.intelligence = intelligence;
+        this.defence = defence;
+        this.resist = resist;
+        this.value = value;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public int getResist() {
+        return resist;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+}
