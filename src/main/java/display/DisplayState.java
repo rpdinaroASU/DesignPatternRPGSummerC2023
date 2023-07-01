62dcb887557b660ec068148df679cca797bc312e
Ryan Dinaro <rpdinaro@asu.edu>
2023-06-30 20:14:30 -0600

    Add state display

---
+++ src/main/java/display/DisplayState.java
@@ -0,0 +1,6 @@
+package display;
+
+public interface DisplayState {
+    void setUpScene();
+    void displayScene();
+}
---
+++ src/main/java/display/IntroState.java
@@ -0,0 +1,13 @@
+package display;
+
+public class IntroState implements DisplayState {
+    @Override
+    public void setUpScene() {
+
+    }
+
+    @Override
+    public void displayScene() {
+
+    }
+}
